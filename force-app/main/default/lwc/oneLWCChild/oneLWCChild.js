0eab67baf2246b9373108345b17ec0e66127d266
kartik1801 <kartikdhawan1801@gmail.com>
2022-08-26 14:20:51 +0530

    up

--- force-app/main/default/lwc/oneLWCChild/oneLWCChild.js
+++ force-app/main/default/lwc/oneLWCChild/oneLWCChild.js
@@ -3,4 +3,9 @@ import { api, LightningElement } from 'lwc';
 export default class OneLWCChild extends LightningElement {
     // with out @api decorator parent cant access / modify child's properties
     @api item = "Hello from Child";
+
+    @api
+    handleChange(){
+        this.item = "button was pressed";
+    }
 }
--- force-app/main/default/lwc/oneLWCParent/oneLWCParent.js
+++ force-app/main/default/lwc/oneLWCParent/oneLWCParent.js
@@ -2,4 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class OneLWCParent extends LightningElement {
     item = "Hello from Parent";
+
+    handleClick(){
+        const child = this.template.querySelector("c-one-l-w-c-child");
+        child.handleEventChage();
+    }
 }
