c7944b1a8f2496d3e12211be187d2762bf79dce3
kartik1801 <kartikdhawan1801@gmail.com>
2022-08-26 14:15:44 +0530

    up

---
+++ force-app/main/default/lwc/oneLWCChild/oneLWCChild.js
@@ -0,0 +1,6 @@
+import { api, LightningElement } from 'lwc';
+
+export default class OneLWCChild extends LightningElement {
+    // with out @api decorator parent cant access / modify child's properties
+    @api item = "Hello from Child";
+}
